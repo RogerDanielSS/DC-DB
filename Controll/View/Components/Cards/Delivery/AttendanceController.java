282d660d05ef133442fb947ce7283e5ef8142daf
RogerDanielSS <danielsimoesp@outlook.com>
2022-11-22 17:46:05 -0300

    wip - updating delivery

---
+++ Controll/View/Components/Cards/Delivery/AttendanceController.java
@@ -0,0 +1,71 @@
+package Controll.View.Components.Cards.Delivery;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class AttendanceController implements Initializable {
+
+  @FXML
+  private Button addNew;
+
+  @FXML
+  private VBox Cards;
+
+  String products[] = { "Areia", "Arenoso", "Cascalho", "Brita", "Areia branca" };
+
+  /**
+   * @param card : receives a card and adds it to interface
+   */
+  private void addCard(AnchorPane card) {
+    Cards.getChildren().add(card);
+    //Cards.setPrefHeight(Cards.getPrefHeight() + 150);
+  }
+
+  /**
+   * Creates a new delivery card
+   * 
+   * @param address     : delivery address
+   * @param product     : product to be delivery
+   * @param quantity    : quantity of the product in meters
+   * @param responsible : person reponsible for the delivery
+   * @param status      : delivery status
+   * @return : a new card
+   */
+  private AnchorPane createDeliveryCard(String address, String product, String quantity) {
+    AnchorPane deliveryCard = new AnchorPane();
+
+    try {
+      URL component_url = getClass().getResource("/View/Components/Cards/Delivery/Delivery.fxml");
+      FXMLLoader fxmlLoader = new FXMLLoader();
+
+      deliveryCard = fxmlLoader.load(component_url.openStream());
+
+      DeliveryController DC = (DeliveryController) fxmlLoader.getController();
+
+      DC.setAddress(address);
+      DC.setProduct(product);
+      DC.setQuantity(quantity);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return deliveryCard;
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+
+    addNew.setOnAction(event -> { 
+      AnchorPane sampleCard = createDeliveryCard("sample", "sample", "sample");
+      addCard(sampleCard);
+    }); 
+  }
+}
